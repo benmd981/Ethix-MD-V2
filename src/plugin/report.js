5dc99390d9b04362ae5075698f4d9d121080d316
benmd981 <vnimsara492@gmail.com>
2024-10-20 12:18:58 +0530

    Update report.js

--- src/plugin/report.js
+++ src/plugin/report.js
@@ -2,16 +2,16 @@ import config from '../../config.cjs';
 
 const report = async (m, gss) => {
   const reportedMessages = {};
-  const devlopernumber = '919142294671';
+  const devlopernumber = '911111111';
 const prefix = config.PREFIX;
 const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  const validCommands = ['bug', 'report', 'request']; 
+  const validCommands = ['menu']; 
   
   if (validCommands.includes(cmd)) {
     
-  if (!text) return m.reply(`Example: ${prefix + cmd} hi dev play command is not working`);
+  if (!text) return m.reply(`suvi tech youtube  https://whatsapp.com/channel/0029VaicAPo2P59qng5LjY0m 👋`);
 
     const messageId = m.key.id;
 
@@ -21,8 +21,8 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
     reportedMessages[messageId] = true;
 
-    const textt = `*| REQUEST/BUG |*`;
-    const teks1 = `\n\n*User*: @${m.sender.split("@")[0]}\n*Request/Bug*: ${text}`;
+    const textt = `*| menu |*`;
+    const teks1 = `\n\n*User*: @${m.sender.split("@")[0]}\n*menu*: ${text}`;
     const teks2 = `\n\n*Hi ${m.pushName}, your request has been forwarded to my Owners.*\n*Please wait...*`;
 
     // Send the message to the first owner in the `owner` array
@@ -34,7 +34,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     });
 
     // Send a reply to the user
-    m.reply("Tʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ʏᴏᴜʀ ʀᴇᴘᴏʀᴛ. Iᴛ ʜᴀs ʙᴇᴇɴ ꜰᴏʀᴡᴀʀᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴏᴡɴᴇʀ. Pʟᴇᴀsᴇ ᴡᴀɪᴛ ꜰᴏʀ ᴀ ʀᴇsᴘᴏɴsᴇ.");
+    m.reply("Tʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ʏᴏᴜʀ menu. Iᴛ ʜᴀs ʙᴇᴇɴ ꜰᴏʀᴡᴀʀᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴏᴡɴᴇʀ. Pʟᴇᴀsᴇ ᴡᴀɪᴛ ꜰᴏʀ ᴀ ʀᴇsᴘᴏɴsᴇ.");
    }
 };
 
